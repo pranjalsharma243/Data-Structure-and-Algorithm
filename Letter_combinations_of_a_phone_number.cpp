b09209be104a5df3a2ef5cdfa9fe6ca1a3948baf
Pranjal Sharma <69204820+pranjalsharma243@users.noreply.github.com>
2023-04-04 01:58:17 +0530

    Update Letter_combinations_of_a_phone_number.cpp
    Used map instead of a vector

--- Letter_combinations_of_a_phone_number.cpp
+++ Letter_combinations_of_a_phone_number.cpp
@@ -1,44 +1,41 @@
 class Solution {
 public:
-void solve(string digits, vector<string> &mapping, vector<string> &ans,int index,string &output)
+void solve(vector<string> &ans,int index,string digits,string output,unordered_map<int,string> &mapping)
 {
-    //Base Case
-    if(index>=digits.length())
-    {
-        ans.push_back(output);
-        return;
-    }
-    int digit=digits[index]-'0';
-    string value=mapping[digit];
-    for(int i=0;i<value.length();i++)
-    {
-        char ch=value[i];
-        output.push_back(ch);
-        solve(digits,mapping,ans,index+1,output);
-        //Backtrack
-        output.pop_back();
-    }
-
+//Base case
+if(index>=digits.length())
+{
+    ans.push_back(output);
+    return;
+}
+int digit=digits[index]-'0';
+string value=mapping[digit];
+for(int i=0;i<value.length();i++)
+{
+    char ch=value[i];
+    solve(ans,index+1,digits,output+ch,mapping);
 
 }
-    vector<string> letterCombinations(string digits) {
-        vector<string> ans;
-        if(digits.length()==0)
-        return ans;
-        vector<string> mapping(10);
-        mapping[2]="abc";
-        mapping[3]="def";
-        mapping[4]="ghi";
-        mapping[5]="jkl";
-        mapping[6]="mno";
-        mapping[7]="pqrs";
-        mapping[8]="tuv";
-        mapping[9]="wxyz";
-        int index=0;
-        string output="";
-        solve(digits,mapping,ans,index,output);
+}
 
-     return ans;
+
+    vector<string> letterCombinations(string digits) {
+    vector<string> ans;
+    if(digits.length()==0)
+    return ans;
+    int index=0;
+    string output="";
+    unordered_map<int,string> mapping;
+    mapping[2]="abc";
+    mapping[3]="def";
+    mapping[4]="ghi";
+    mapping[5]="jkl";
+    mapping[6]="mno";
+    mapping[7]="pqrs";
+    mapping[8]="tuv";
+    mapping[9]="wxyz";
+    solve(ans,index,digits,output,mapping);
+    return ans;
         
     }
 };
