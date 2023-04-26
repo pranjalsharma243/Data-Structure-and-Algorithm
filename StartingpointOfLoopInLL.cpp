c0a159074c87ea38ce2f9468bdaf2e245987631d
Pranjal Sharma <69204820+pranjalsharma243@users.noreply.github.com>
2023-04-27 01:28:59 +0530

    Update StartingpointOfLoopInLL.cpp

--- StartingpointOfLoopInLL.cpp
+++ StartingpointOfLoopInLL.cpp
@@ -10,50 +10,8 @@ Node(int data)
   this->data=data;
   this->next=NULL;
 }
-
 };
-int getlen(Node* &head)
-{
-Node* temp=head;
-int len=0;
-while(temp!=NULL)
-{
-  len++;
-  temp=temp->next;
-}
-return len;
-}
-Node* reverseIngroups(Node* &head, int k)
-{
-if(head==NULL)
-{
-  return head;
-}
-int len=getlen(head);
-if(k>len)
-{
-return head;
-}
-//k<=len
-Node* prev=NULL;
-Node* curr=head;
-Node* forward=curr->next;
-int count=0;
-while(count<k)
-{
-forward=curr->next;
-curr->next=prev;
-prev=curr;
-curr=forward;
-count++;
-}
-if(forward!=NULL)
-{
- head->next=reverseIngroups(forward,k);
-}
-return prev;
 
-}
 void print(Node* &head)
 {
 Node* temp=head;
@@ -64,7 +22,42 @@ temp=temp->next;
 }
 
 }
+Node* starting(Node* &head)
+{
+if(head==NULL)
+{
+  return NULL;
+}
+Node* slow=head;
+Node* fast=head;
 
+while(fast!=NULL)
+{
+  fast=fast->next;
+  if(fast!=NULL)
+  {
+    fast=fast->next;
+    slow=slow->next;
+  }
+  if(fast==slow)
+  {
+    slow=head;
+    break;
+  }
+}
+
+while(slow!=fast)
+{
+  slow=slow->next;
+  fast=fast->next;
+}
+return slow;
+
+
+
+
+
+}
 int main() {
   Node* head=new Node(10);
   Node* second=new Node(20);
@@ -83,9 +76,8 @@ int main() {
   sixth->next=seventh;
   seventh->next=eighth;
   eighth->next=ninth;
-  ninth->next=NULL;
-  int k=3;
-  head=reverseIngroups(head,k);
-  print(head);
+  ninth->next=fifth;
+ cout<< starting(head)->data<<endl;
+
   return 0;
 }
